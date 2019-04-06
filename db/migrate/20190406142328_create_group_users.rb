8e7fb1ea66e464f1316602db40c82aad74df2ea4
ucchiii <shop.rouro@gmail.com>
2019-04-06 23:29:07 +0900

    Add-Group_model,controller

---
+++ db/migrate/20190406142328_create_group_users.rb
@@ -0,0 +1,8 @@
+class CreateGroupUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :group_users do |t|
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
+    end
+  end
+end
