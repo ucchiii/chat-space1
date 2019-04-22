c0784f52d9a012aeb27bdf25aea92cfa315ae9a1
ucchiii <shop.rouro@gmail.com>
2019-04-23 05:24:41 +0900

    Add-IncrementalSearch-End

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,15 +1,26 @@
 $(function() {
-  function buildHTML(user){
+  function buildUsersHTML(user){
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ user.name }</p>
                   <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${ user.id }" data-user-name="${ user.name }">追加</div>
                 </div>`
+    $('#user-search-result').append(html);
     return html
   }
 
   function appendErrMsgToHTML(msg) {
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${ msg }</p>
+                </div>`
+    $('#user-search-result').append(html);
+    return html
+  }
+
+  function buildAddMemberHTML(userId, userName) {
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                  <input name='group[user_ids][]' type='hidden' value='${ userId }'>
+                  <p class='chat-group-user__name'>${ userName }</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
                 </div>`
     return html
   }
@@ -25,16 +36,17 @@ $(function() {
       contentType: false
     })
     .done(function(data){
+      console.log(data)
       $('#user-search-result').empty();
       if (data.length !== 0) {
         data.forEach(function(data){
-          html = buildHTML(data);
+          console.log(data)
+          html = buildUsersHTML(data);
         });
       }
       else {
         html = appendErrMsgToHTML("一致するユーザーが見つかりません");
       }
-      $('#user-search-result').append(html);
     })
     .fail(function(data){
       alert('ユーザー検索に失敗しました');
@@ -43,4 +55,18 @@ $(function() {
       $('#user-search-result').removeAttr('disabled');
     })
   })
+
+  $(document).on("click", '.user-search-add', function () {
+    var userId = $(this).attr("data-user-id");
+    var userName = $(this).attr("data-user-name");
+
+    $(this).parent().remove();
+
+    var html = buildAddMemberHTML(userId, userName);
+    $('#chat-group-users').append(html);
+  });
+
+  $(document).on("click", '.user-search-remove', function () {
+    $(this).parent().remove();
+  });
 })
